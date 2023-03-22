c57d48da9b602ff4027a6104778ccce3496430a6
luoxpan <128618329+luoxpan@users.noreply.github.com>
2023-03-22 23:27:02 +0800

    基础实现，还没有实现线程池动态改变大小添加 .gitattributes、.gitignore 和 README.md。

---
+++ ThreadPool.cpp
@@ -0,0 +1,87 @@
+﻿#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(int init_size):
+    m_init_threads_size(init_size),
+    m_mutex(),
+    m_cond(),
+    m_is_running(false)
+{
+    start();
+}
+
+ThreadPool::~ThreadPool()
+{
+    if (m_is_running)
+    {
+        stop();
+    }
+}
+
+void ThreadPool::start()
+{
+    assert(m_threads.empty());
+    m_is_running = true;
+    m_threads.reserve(m_init_threads_size);
+    for (int i = 0; i < m_init_threads_size; ++i)
+    {
+        m_threads.push_back(new std::thread(std::bind(&ThreadPool::thread_loop, this)));
+    }
+
+}
+
+void ThreadPool::stop()
+{
+    std::cout << "thread_pool stop. " << std::endl;
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_is_running = false;
+        m_cond.notify_all();//通知所有挂起线程
+    }
+
+    for (threads_t::iterator it = m_threads.begin(); it != m_threads.end(); ++it)
+    {
+        (*it)->join();
+        delete* it;
+    }
+    m_threads.clear();
+}
+
+
+void ThreadPool::thread_loop()
+{
+    std::cout << "thread " << gettid() << " start" << std::endl;
+    while (m_is_running)
+    {
+        task_t task = take();
+        if (task)
+        {
+            task();
+        }
+    }
+    std::cout << "thread " << gettid() << " exit." << std::endl;
+}
+
+void ThreadPool::add_task(const task_t& task)
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_tasks.push_back(task);
+    m_cond.notify_one();
+}
+
+ThreadPool::task_t ThreadPool::take()
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    //这里的wait必须在while循环内部,不能是if
+    while (m_tasks.empty() && m_is_running)
+    {
+        m_cond.wait(lock);
+    }
+    task_t task;
+    auto size = m_tasks.size();
+    if (!m_tasks.empty() && m_is_running)
+    {
+        task = m_tasks.front();
+        m_tasks.pop_front();
+    }
+    return task;
+}
---
+++ ThreadPool.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <deque>
+#include <functional>
+#include <iterator>
+#include <cassert>
+#include <mutex>
+#include <condition_variable>
+
+class  ThreadPool{
+public:
+	typedef std::function<void()> task_t;
+
+	ThreadPool(int init_size = 3);
+	~ThreadPool();
+
+	void stop();
+	void add_task(const task_t&);
+
+private:
+	ThreadPool(const ThreadPool&)=delete;
+	const ThreadPool& operator=(const ThreadPool&)=delete;
+
+	bool is_running() { return m_is_running; }
+	void start();
+
+	void thread_loop();
+	task_t take();
+
+	typedef std::vector<std::thread*> threads_t;
+	typedef std::deque<task_t> tasks_t;
+
+	int m_init_threads_size;
+
+	threads_t m_threads;
+	tasks_t m_tasks;
+
+	std::mutex m_mutex;
+	std::condition_variable m_cond;
+	bool m_is_running;
+};
